65a4e8a87a10e92ec01f2b44c2f4f724170d6c66
Filip Kajetaniak <filip.kajetaniak@codequest.com>
2019-12-03 18:37:57 +0100

    Add basic webpack configuration

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,3 @@
+import React, { FC } from "react";
+
+export const TestComponent: FC = () => <h1>Hello world!</h1>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
-const path = require(path);
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js "
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
   },
-  module: {},
-  plugins: []
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.[contentHash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        include: path.resolve(__dirname, "src")
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      inject: true
+    }),
+    new CleanWebpackPlugin()
+  ]
 };
