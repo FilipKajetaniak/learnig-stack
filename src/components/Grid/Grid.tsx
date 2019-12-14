cc9d07a1767a3995f72629f5a46e390427bc2d4b
Filip Kajetaniak <kajetaniakkfilip@gmail.com>
2019-12-14 22:32:50 +0100

    Add basic color palette

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from "react";
 
+import { SideMenu } from "components/SideMenu/SideMenu";
+
 import style from "./Grid.module.scss";
 
-export const Grid: FC = () => <main className={style.container}>Grid</main>;
+export const Grid: FC = () => (
+  <main className={style.container}>
+    <SideMenu />
+  </main>
+);
---
+++ src/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+import style from "./SideMenu.module.scss";
+
+export const SideMenu: FC = () => {
+  return <aside className={style.container}>Side menu</aside>;
+};
